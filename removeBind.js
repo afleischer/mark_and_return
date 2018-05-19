4e091930aa66f38943f91c679a530a486e2d9b2f
Aidan Fleischer <afleischer1337@gmail.com>
2018-05-19 01:03:05 -0700

    Delete removeBind.js
    Deleted file so I can merge local copies in

--- removeBind.js
+++
@@ -1,8 +0,0 @@
-
-//Get DOM element of the most recently added 
-var removeBind = document.getElementById("removeBind").addEventListener("click", () => {
-
-inputFields=document.getElementsByClassName("bindInput");
-last_element = inputFields[inputFields.length - 1];
-document.removeChild(last_element);
-})
